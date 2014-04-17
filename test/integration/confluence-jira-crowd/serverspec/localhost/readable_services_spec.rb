705e784badf8c97f03793177d2bb145392d50499
Emmanuel Sciara <emmanuel.sciara@gmail.com>
2014-04-18 01:15:50 +0200

    busser cumcumber and serverspec tests passing

--- test/integration/confluence-jira-crowd/serverspec/localhost/readable_services_spec.rb
+++ test/integration/confluence-jira-crowd/serverspec/localhost/readable_services_spec.rb
@@ -4,6 +4,6 @@ require 'spec_helper'
 describe 'confluence-jira-crowd::default' do
 
   describe command('/etc/init.d/confluence status') do
-    it { should return_stdout '. confluence is running' }
+    it { should return_stdout '* confluence is running' }
   end
 end
