dd1b8f4282dd099ab2b8ac4c669db1fba62c644e
Emmanuel Sciara <emmanuel.sciara@gmail.com>
2014-05-08 01:02:06 +0200

    removed nokogiri that caused pbs on digitalocean test

--- test/integration/confluence-jira-crowd/cucumber/spec_helper.rb
+++ test/integration/confluence-jira-crowd/cucumber/spec_helper.rb
@@ -16,11 +16,3 @@ rescue LoadError
   Gem::DependencyInstaller.new.install('faraday')
   require 'faraday'
 end
-
-begin
-  require 'nokogiri'
-rescue LoadError
-  require 'rubygems/dependency_installer'
-  Gem::DependencyInstaller.new.install('nokogiri')
-  require 'nokogiri'
-end
--- test/integration/confluence-jira-crowd/cucumber/step_definitions/confluence_up_steps.rb
+++ test/integration/confluence-jira-crowd/cucumber/step_definitions/confluence_up_steps.rb
@@ -1,6 +1,5 @@
 # encoding: UTF-8
 require 'faraday'
-require 'nokogiri'
 require 'socket'
 
 Given(/^the url of Confluences home page$/) do
@@ -13,9 +12,10 @@ When(/^a web user browses to the url$/) do
                            :ssl => { :verify => false }) do |faraday|
     faraday.adapter Faraday.default_adapter
   end
-  @title = Nokogiri::HTML(connection.get('/setup/setuplicense.action').body).title
+  @page_body = connection.get('/setup/setuplicense.action').body
 end
 
 Then(/^the page should have the title "(.*?)"$/) do |title|
-  expect(@title).to match(/#{title}/)
+  page_title = @page_body.match(/<title>(.*?)<\/title>/)[1]
+  expect(page_title).to match(title)
 end
